f8161a3cdbe228cd263aad6bc3cd75e254fcd1c7
Irina Khaustova <irina170580@yandex.ru>
2021-09-26 19:47:09 +0300

    task completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,21 @@
+'use strict';
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
+  let x;
+  let x1;
   // код для задачи №1 писать здесь
+  let d = (b**2) - (4*a*c);
+  //if (d < 0) {
+  //}
+  if (d === 0) {
+    x = -b/(2*a);
+    arr.push(x);
+  }
+  else if (d > 0){
+  	x = (-b + Math.sqrt(d))/(2*a);
+  	x1 = (-b - Math.sqrt(d))/(2*a);
+    arr.push(x, x1);
+  };
   return arr; // array
 }
 
