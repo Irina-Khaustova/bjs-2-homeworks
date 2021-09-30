10fbb82cff32fb6483e66765c36b3cfd145c5d90
Irina Khaustova <irina170580@yandex.ru>
2021-09-30 23:01:44 +0300

    task completed 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -17,12 +17,24 @@ function solveEquation(a, b, c) {
     arr.push(x, x1);
   };
   return arr; // array
-}
+};
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
-
-  // код для задачи №2 писать здесь
-
+  if (isNaN(percent)) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } 
+  else if (isNaN(contribution)) {
+  	return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  }
+  else if (isNaN(amount)) {
+  	return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  };
+  let summCredit = +amount - +contribution;
+  let date1 = date;
+  let date2 = Date.now();
+  let loanDuration = Math.floor((date1 - date2)/(1000*60*60*24*30));
+  totalAmount = +((loanDuration*summCredit*(1/12*(+percent)/100 + (1/12*(+percent)/100)/(((1 + (1/12*(+percent)/100))**loanDuration) - 1))).toFixed(2));
   return totalAmount;
 }
+
