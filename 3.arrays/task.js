5be588701918e57cfbeac19ac8481b3d391d38c8
Irina Khaustova <Irina170580@yandex.ru>
2021-10-08 21:14:54 +0300

    3.arrays-version2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,13 +1,7 @@
 function compareArrays(arr1, arr2) {
-  let result;
-  result = arr1.every(i => arr1[i] === arr2[i]);
-  return result; // boolean
+  return (arr1.length === arr2.length) && (arr1.every((elem, i) => (arr1[i] === arr2[i])));
 };
 
 function advancedFilter(arr) {
-  let resultArr;
-  arr1 = arr.filter(i => i > 0);
-  arr2 = arr1.filter(i => i % 3 === 0);
-  resultArr = arr2.map(i => i*10);
-  return resultArr; // array
+  return arr.filter(i => i > 0).filter(i => i % 3 === 0).map(i => i*10);
 };
