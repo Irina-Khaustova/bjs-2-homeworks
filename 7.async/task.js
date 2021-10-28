ecbdbd7d02cea9c1f7ee871b5adcc997a7ce2728
Irina Khaustova <Irina170580@yandex.ru>
2021-10-28 19:27:23 +0300

    7.async-2

--- 7.async/task.js
+++ 7.async/task.js
@@ -2,31 +2,71 @@ class AlarmClock {
 	constructor () {
 		this.alarmCollection = [];
 		this.timerId = null;
-	}; 
-
-    addClock (date, callback, id) {
+	};
+	addClock (time, callback, id){
       if (id === undefined) {
         throw new Error("не передан параметр id");
       }
       if (this.alarmCollection.find(item => item.id === id)) {
-        console.error('такой id уже существует')
-      }
-      this.alarmCollection.push({id, date, callback})
+        console.error('будильник с таким id уже существует')
+      };
+      this.alarmCollection.push({id, time, callback})
 	};
 
-	removeClock (id) {
+  removeClock (id) {
+  	  let alarmLength1 = this.alarmCollection.length;
 	  let delClock = this.alarmCollection.find(item => item.id === id);
       this.alarmCollection.splice(this.alarmCollection.indexOf(delClock), 1);
-	};
+      let alarmLength2 = this.alarmCollection.length;
+      return (alarmLength1 === alarmLength2); 
+	}
 
-	getCurrentFormattedTime () {
-      return new Date();
-    };
-
-    start () {
-      checkClock () {
-      
-    }
+  getCurrentFormattedTime () {
+    let time = new Date ()
+    return time.toLocaleTimeString([], {timeZone: 'Europe/Moscow', hour: 'numeric', minute: 'numeric', hour12: false});
   }
 
-   };
+  start () {
+    function checkClock (ddt) {
+      if (this.getCurrentFormattedTime() === ddt.time) {
+        return ddt.callback();
+      }
+    }
+    if (this.timerId === null) {
+      const check = setInterval(function () {
+        for (let i = 0; i < this.alarmCollection.length; i++) {
+          this.checkClock(this.alarmCollection[i]);
+        }
+      }, 3000); 
+      return check;
+    }  
+  };
+
+  stop () {
+    if (this.timerId) {
+      clearsetInterval(this.check);
+      this.timerId = null;
+    }
+  }
+ 
+  printAlarms () {
+    this.alarmCollection.forEach((item) => console.log('id:' + item.id + '; ' + 'time ' + item.time));
+  }
+
+  clearAlarms () {
+    this.stop();
+    this.alarmCollection.length = 0;
+  }
+   }
+ 
+ function testCase () {
+ 	let alarmClock = new AlarmClock();
+ 	alarmClock.addClock('7:47', () => console.log('давай вставай'), 1);
+ 	alarmClock.addClock('7:48', () => {console.log('давай вставай'); alarmClock.removeClock (2)}, 2);
+ 	alarmClock.addClock('7:49', () => {console.log('давай вставай'); alarmClock.stop(); alarmClock.clearAlarms(); alarmClock.printAlarms ()}, 3);
+ 	alarmClock.printAlarms ();
+ 	alarmClock.start();
+ };
+
+ testCase();
+
