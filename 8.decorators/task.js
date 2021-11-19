66104c3fa7013bfa2ec97105191e33fb45392832
Irina Khaustova <Irina170580@yandex.ru>
2021-11-19 21:54:24 +0300

    8.decorators-version2

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,59 +1,73 @@
 function cachingDecoratorNew(func) {
- let cache = [];
+	
+  let cache = [];
+
   function wrapper(...args) {
-  	hash = args.join(',');
-    let idx = cache.findIndex((item)=> item.hash == hash)
-  	if (idx !== -1) {
+  	
+    hash = args.join(',');
+    let idx = cache.findIndex((item)=> item.hash == hash);
+    if (idx !== -1) {
       console.log('Из кэша: ' + cache[idx].value);
       return 'Из кэша: ' + cache[idx].value;
-  	}
-      let result = func(...args);
-      cache.push({hash, value:result});
-      if (cache.length > 5) {
+  	};
+    let result = func(...args);
+    cache.push({hash, value:result});
+    if (cache.length > 5) {
       cache.shift();
     }
-  	  console.log('Вычисляем: ' + result);
-      return 'Вычисляем: ' + result;
+  	console.log('Вычисляем: ' + result);
+    return 'Вычисляем: ' + result;
   };  
+
   return wrapper;
 };  
   
 
 function debounceDecoratorNew(func) {
+
   let flag = false;
   let timeout;
+  
+  return function (...args) { 
 
-  return function (...args) {  
-    clearTimeout(timeout); 
-    if (flag === false) {
+    clearTimeout(timeout);
+    if (flag === false && !timeout) {
       func.apply(this, args);
+      console.log(args)
       flag = true;
-    }         
-    timeout = setTimeout(() => {
-      flag = false;             
-    }, ms);
+    } else { 
+      clearTimeout(timeout);        
+      timeout = setTimeout(() => {
+        func.apply(this, args);
+        flag = false;
+      },ms);
+    };
   };
-}
+};
 
 function debounceDecorator2(func) {
+
   let flag = false;
   let timeout;
-  let i = 0;
+  wrapper.count = 0;
 
-  return function wrapper (...args) {
-    wrapper['count'];  
-    clearTimeout(timeout); 
-    if (flag === false) {
+  function wrapper (...args) { 
+    
+    clearTimeout(timeout);
+    if (flag === false && !timeout) {
       func.apply(this, args);
       flag = true;
-      i = i + 1;
-      wrapper['count'] = i;
-      console.log(wrapper['count'], i);
-    }         
+    } else { 
+      clearTimeout(timeout);        
       timeout = setTimeout(() => {
-      flag = false;             
-       }, 
-      ms);
+        func.apply(this, args);
+        flag = false;
+      },ms);
+    };
+     
+    wrapper.count += 1;
   };
+
   return wrapper;
-}
+  console.log(wrapper.count)
+};
